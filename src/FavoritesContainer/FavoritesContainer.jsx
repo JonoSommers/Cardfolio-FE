b98d53a1056e843da7b45043e59cfd6ec685ea89
Jono Sommers <jonosommers95@gmail.com>
2025-03-05 19:46:29 -0600

    Update FavoritesContainer.jsx
    removed commented out code

--- src/FavoritesContainer/FavoritesContainer.jsx
+++ src/FavoritesContainer/FavoritesContainer.jsx
@@ -2,9 +2,6 @@ import "./FavoritesContainer.css";
 import { useState } from "react";
 
 function FavoritesContainer() {
-    // const [favorites, setfavorites] = useState[null]
-
-
     return (
         <div className="favorites-container">
         </div>
