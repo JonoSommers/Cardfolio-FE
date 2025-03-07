157fdfa3b003683dbcd3c6eecbfde087cf01ae5d
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2025-03-07 11:14:10 -0600

    Home page fixes

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -29,11 +29,11 @@ function HomePage({ userData, setUserData }) {
   const favoriteCards = allCards.filter(card => card.data.attributes.favorite.favorite === true);
 
   return (
-    <div className="card-container">
-      <div className="card-name">{displayName}</div>
+    <div className="home-container">
+      <div className="home-name">{displayName}</div>
 
-      <div className="card-image">
-        <section className="buttons-container">
+      <div className="home-image">
+        <section className="home-buttons-container">
           <Link to={'/pokemon_search'}>
             <button className="homeViewButton">Pokemon</button>
           </Link>
@@ -48,9 +48,10 @@ function HomePage({ userData, setUserData }) {
               <button className="bindersButton">{binder.name}</button>
             </Link>
           ))}
-            {userData.attributes.binders.length === 1 ? <Link to='/createbinder'><button className="createbinder">Create A New Binder</button></Link> : null }
+            {userData.attributes.binders.length === 1 ? <Link to='/createbinder'><button className="createBinder">Create A New Binder</button></Link> : null }
         </section>
-
+      </div>
+        <section className="homeDesc">
         {favoriteCards.length > 0 && (
           <section className="favorites-container">
             <h2>Favorite Cards</h2>
@@ -63,12 +64,12 @@ function HomePage({ userData, setUserData }) {
             </div>
           </section>
         )}
-				<div>
+          </section>
+				<div className="logOut">
 					<Link to={"/"}>
 						<button className="logout-button">Logout</button>
 					</Link>
 				</div>
-      </div>
     </div>
   );
 }
