d771e3f3117f62a0e893d81ce6fded20d757fac2
Jono Sommers <jonosommers95@gmail.com>
2025-03-07 15:14:41 -0600

    fixed binder rename issue

--- src/BinderView/BinderView.jsx
+++ src/BinderView/BinderView.jsx
@@ -1,17 +1,17 @@
 import { useParams, Link, useNavigate } from 'react-router-dom'
-import { useState } from 'react'
+import { useState, userEffect } from 'react'
 import homeIcon from '../icons/home.png'
 import searchIcon from '../icons/search.png'
 import './BinderView.css'
 
-function BinderView({ userData }) {
+function BinderView({ userData, setUserData }) {
 	const navigate = useNavigate();
 	const [search, setSearch] = useState('')
 	const [showInput, setShowInput] = useState(false);
 	const [newName, setNewName] = useState("")
 	const userName = userData.attributes.username
-  const binderName = useParams().bindername
-  const binder = userData.attributes.binders.find(binder => binder.name === binderName);
+  	const binderName = useParams().bindername
+  	const binder = userData.attributes.binders.find(binder => binder.name === binderName);
 	const filteredCards = binder.binders_cards.filter((card) => (
 		card.data.attributes.card.name.toLowerCase().startsWith(search.toLowerCase())
 	))
@@ -27,7 +27,6 @@ function BinderView({ userData }) {
     	)
 	})
 	
-
 	function renameBinder() {
 		fetch(`http://localhost:3000/api/v1/users/${userData.id}/binders/${binder.id}`,{
 			method: "PATCH",
@@ -41,47 +40,47 @@ function BinderView({ userData }) {
 		.then(response => response.json())
 	}
 
-
 	const handleRename = (event) => {
 		event.preventDefault();
 		renameBinder()
 		setShowInput(!showInput)
 		navigate(`/${userName}`)
-	  };
+	};
+
 
 	return (
-    <section className="binderView">
-        <div className="top">
-            <Link to={`/${userName}`}>
-				<img className="homeIcon" src={homeIcon} alt="home icon" />
-			</Link>
-				<h1 className="binderName">{binderName}</h1>
-				<button onClick={() => setShowInput(!showInput)} className="renameButton">Rename Binder</button>
-				{showInput && (
-					<form onSubmit={handleRename}>
-						<input 
-						type="text"
-						placeholder='New Name'
-						className='newBinderName'
-						value={newName}
-						onChange={(event) => setNewName(event.target.value)}/>
-						<button type="submit" >Submit</button>
-					</form>
-				)}
-			<img className="searchIcon" src={searchIcon} alt="search icon" /> 
-			<input 
-				className="searchBar"
-				type="text"
-				placeholder="Search Name..."
-				value={search}
-				onChange={(event) => setSearch(event.target.value)}
-			/>
-                </div>
-			<section className="binderCards">
-				{searchCards}
-			</section>
-    </section>
-  )
+		<section className="binderView">
+			<div className="top">
+				<Link to={`/${userName}`}>
+					<img className="homeIcon" src={homeIcon} alt="home icon" />
+				</Link>
+					<h1 className="binderName">{binderName}</h1>
+					<button onClick={() => setShowInput(!showInput)} className="renameButton">Rename Binder</button>
+					{showInput && (
+						<form onSubmit={handleRename}>
+							<input 
+							type="text"
+							placeholder='New Name'
+							className='newBinderName'
+							value={newName}
+							onChange={(event) => setNewName(event.target.value)}/>
+							<button type="submit" >Submit</button>
+						</form>
+					)}
+				<img className="searchIcon" src={searchIcon} alt="search icon" /> 
+				<input 
+					className="searchBar"
+					type="text"
+					placeholder="Search Name..."
+					value={search}
+					onChange={(event) => setSearch(event.target.value)}
+				/>
+					</div>
+				<section className="binderCards">
+					{searchCards}
+				</section>
+		</section>
+	)
 }
 
 export default BinderView
