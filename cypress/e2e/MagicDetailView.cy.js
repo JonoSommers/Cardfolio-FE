6012cb5e3bcc38858ce9417d8927a82c7baa7b51
Joel Davalos <jsdavalos@gmail.com>
2025-03-08 10:45:17 -0700

    Test: Refactoring test for adding a selected card

---
+++ cypress/e2e/MagicDetailView.cy.js
@@ -0,0 +1,61 @@
+describe('MagicDetailView Spec', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'http://localhost:3000/api/v1/users', {
+      statusCode: 200,
+      fixture: 'all_users_data.json'
+    }).as('fetchAllUsers');
+
+    cy.intercept('GET', 'http://localhost:3000/api/v1/users/2', {
+      statusCode: 200,
+      fixture: 'magicman122_binderview_data.json'
+    }).as('fetchUserData');
+
+    cy.intercept('GET', 'https://api.magicthegathering.io/v1/cards', {
+      fixture: 'magic_cards_view.json'
+    }).as('fetchCardsView')
+
+    cy.intercept('GET', 'https://api.magicthegathering.io/v1/cards/1', {
+      fixture: 'magic_card_view.json'
+    }).as('fetchCardDetails')
+
+    cy.visit('https://cardfolio-fe.onrender.com')
+    cy.wait('@fetchAllUsers')
+    cy.get('input').type('MagicMan122')
+    cy.get('.login_button').click()
+    cy.wait('@fetchUserData')
+    cy.url().should('include', '/MagicMan122')
+    
+    cy.get('.homeViewButton').eq(1).should('contain', "MTG").click()
+    cy.url().should('include', '/mtg_search')
+
+  })
+  
+  it('should display card details correctly', () => {
+    
+    cy.wait('@fetchCardsView')
+    cy.get('.MTGcard').first().click()
+    cy.wait('@fetchCardDetails')
+    cy.get('h1').should('contain', "Ancestor's Chosen")
+    cy.get('img').should('have.attr', 'src', 'http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=130550&type=card')
+    cy.get('.add').should('contain', 'Add To Binder')
+    cy.get('.back > button').should('contain', 'Back')
+  })
+
+  it('should add a card to the selected binder', () => {
+    cy.wait('@fetchCardsView');
+  
+    cy.get('.MTGcard').first().click();
+    cy.wait('@fetchCardDetails');
+  
+    cy.intercept('POST', 'http://localhost:3000/api/v1/users/2/binders/1/binder_cards', {
+      statusCode: 200,
+      fixture: 'magicman122_card_submit.json'
+    }).as('addCard');
+  
+    cy.get('.add').click();
+    cy.wait('@addCard')
+    cy.get('.back > button').click()
+    
+    
+  });
+})
